dfeecd2222b1404da7e76156092410b51363ae2e
Connor119 <62894592+Connor119@users.noreply.github.com>
2022-04-08 22:06:03 +0800

    Delete myFlaskHello directory

--- myFlaskHello/templates/foliumTest.py
+++
@@ -1,7 +0,0 @@
-import folium
-import os
-m = folium.Map(location=[29.488869,106.571034],
-              zoom_start=14)
-
-folium.Marker([29.488869,106.571034], popup='<i>Mt. Hood Meadows</i>').add_to(m)
-m
